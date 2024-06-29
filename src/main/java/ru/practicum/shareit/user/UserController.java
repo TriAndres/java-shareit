2b39e43d7bebdde9264537f76b4b3d3d2243ed79
Andrei <An.S-1@yandex.ru>
2024-06-29 19:00:50 +0300

    add-controllers add user 5

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RequestMapping(path = "/users")
 public class UserController {
     private final UserService userService;
+    
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -13,6 +13,7 @@ import java.util.Objects;
 @Slf4j
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;

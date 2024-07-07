7fb199548b702ae89c7eb04b28f68276cf5f5737
Andrei <An.S-1@yandex.ru>
2024-07-07 11:39:34 +0300

    add-controllers 4

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.exseption.ConflictException;
 import ru.practicum.shareit.user.dto.UserDto;
 
 /**
